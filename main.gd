f7a981771fabca46c078f1299ccff7f5c54ca896
DevenairThomas <deventhomas55@gmail.com>
2025-03-29 16:30:45 -0400

    Added support for laptop work

--- main.gd
+++ main.gd
@@ -10,20 +10,38 @@ func _process(delta):
 	fart_level += fart_rate * delta
 	fart_level = clamp(fart_level, 0, 100)
 	fart_meter.value = fart_level
-	
 	if fart_level >= 100:
 		game_over()
 		
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			release_fart()
-			
-func release_fart():
-	fart_level -= release_rate
-	fart_level = clamp(fart_level, 0, 100)
-	fart_meter.value = fart_level
-	print("Fart released Current level: ", fart_level)
+		if event.is_ctrl_pressed():
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				release_fart(true)
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				release_fart(true)
+		else:
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				release_fart(true)
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				release_fart(false)
+	elif event is InputEventMagnifyGesture:
+		if event.factor > 1.0: #Touchpad fingers spread
+			release_fart(true)
+			print("Hit spread")
+		if event.factor < 1.0: #Touchpad pinch
+			release_fart(false)
+			print("Hit pinch")
+	else:
+		release_fart(false)
+		print("Hit input event screen touch equals false")
+		
+func release_fart(release: bool):
+	if release == true:
+		fart_level -= release_rate
+		fart_level = clamp(fart_level, 0, 100)
+		fart_meter.value = fart_level
+		print("Fart released Current level: ", fart_level)
 	
 func game_over():
 	print("GAME OVER: Player Exploded")
